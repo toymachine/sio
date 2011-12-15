4d5de65f035d886abaaaf6c535488ff81a02751a
Henk Punt <henkpunt@gmail.com>
2011-12-15 16:22:31 +0100

    bla

--- src/sio/test2.clj
+++ src/sio/test2.clj
@@ -1,4 +1,5 @@
 (ns sio.test2
+  (:use [sio.vector])
   (:require [sio.game :as game])
   (:import
    [java.awt Graphics2D Color]
@@ -6,7 +7,7 @@
 
 (set! *warn-on-reflection* true)
 
-(deftype State [x v])
+(deftype State [position velocity])
 
 (deftype Derivative [dx dv])
 
@@ -14,29 +15,28 @@
 (defn spring [k b]
   ;creates an acceleration function for a spring
   (fn [^State state t]
-    (- (* (- k) (.x state)) (* b (.v state)))))
+    (v- (v* (- k) (.position state))
+        (v* b (.velocity state)))))
 
 ;evaluate/integrate form the RK4 integrator
 (defn evaluate [^State initial t dt ^Derivative derivative acceleration]
-  (let [state (State. (+ (.x initial) (* (.dx derivative) dt))
-                      (+ (.v initial) (* (.dv derivative) dt)))]
-    (Derivative. (.v state)
-                 (acceleration state (+ t dt)))))
+  (let [state (State. (v+ (.position initial) (v* dt (.dx derivative)))
+                      (v+ (.velocity initial) (v* dt (.dv derivative))))]
+    (Derivative. (.velocity state) (acceleration state (+ t dt)))))
 
 (defn integrate [^State state t dt acceleration]
-  (let [^Derivative a (evaluate state t 0 (Derivative. 0 0) acceleration)
+  (let [^Derivative a (evaluate state t 0 (Derivative. vector-3d-zero vector-3d-zero) acceleration)
         ^Derivative b (evaluate state t (* dt 1/2) a acceleration)
         ^Derivative c (evaluate state t (* dt 1/2) b acceleration)
         ^Derivative d (evaluate state t dt c acceleration)
-        dxdt (* 1/6 (+ (.dx a) (* 2 (+ (.dx b) (.dx c))) (.dx d)))
-        dvdt (* 1/6 (+ (.dv a) (* 2 (+ (.dv b) (.dv c))) (.dv d)))]
-    (State. (+ (.x state) (* dxdt dt))
-            (+ (.v state) (* dvdt dt)))))
-
+        dxdt (v* 1/6 (v+ (.dx a) (v* 2 (v+ (.dx b) (.dx c))) (.dx d)))
+        dvdt (v* 1/6 (v+ (.dv a) (v* 2 (v+ (.dv b) (.dv c))) (.dv d)))]
+    (State. (v+ (.position state) (v* dt dxdt))
+            (v+ (.velocity state) (v* dt dvdt)))))
 
 ;demo
 (def acceleration
-  (spring 20 1))
+  (spring 20 0.5))
 
 (defn simulate [state t dt]
   (integrate state t dt acceleration))
@@ -46,9 +46,9 @@
     (.setColor Color/black)
     (.fillRect 0 0 w h)
     (.setColor Color/white)
-    (.fillRect (- 200 (.x state)) 100 15 15)))
+    (.fillRect (- 200 (.x (.position state))) (- 200 (.y (.position state))) 15 15)))
 
 (defn start []
-  (game/start 400 400 25 simulate render (State. 200.0 0)))
+  (game/start 400 400 25 simulate render (State. (vector-3d 200.0 100.0 0) vector-3d-zero)))
         
 ;(sim 100  1/10)
